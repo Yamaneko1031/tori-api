0553bbdadcf18c5219721a31b5e964c28e4f0282
Yamane <candoanything1031@gmail.com>
2021-04-23 13:05:42 +0900

    タグ取得追加

--- app/app/api/tag.py
+++ app/app/api/tag.py
@@ -21,3 +21,11 @@ def get_tag():
     if not ret_data:
         raise HTTPException(status_code=404, detail="unknown tag_choices.")
     return ret_data
+
+
+@router.get("/tag/more0", tags=["tag"])
+def get_tag():
+    ret_data = tag_service.get_random_tag_more0()
+    if not ret_data:
+        raise HTTPException(status_code=404, detail="unknown tag_more0.")
+    return ret_data
