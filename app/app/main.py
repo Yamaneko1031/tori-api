ee979203ca71c6d7f50a66331c1e785be90db2e3
Yamane <candoanything1031@gmail.com>
2022-04-21 10:48:19 +0900

    テスト

--- app/app/main.py
+++ app/app/main.py
@@ -15,7 +15,7 @@ def configure(app):
     ]
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
