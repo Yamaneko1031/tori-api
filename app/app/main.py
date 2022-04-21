6172b1951898c5a848b34701a32f4020a8714256
Yamane <candoanything1031@gmail.com>
2022-04-21 21:06:17 +0900

    テスト

--- app/app/main.py
+++ app/app/main.py
@@ -1,41 +1,49 @@
 # main.py
-# from fastapi import FastAPI
-# from fastapi.staticfiles import StaticFiles
-# from starlette.middleware.cors import CORSMiddleware
-# from app.api import user, word, system, tag
-# from app.webapps import admin
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
+from app.api import user, word, system, tag
+from app.webapps import admin
 
 
-# def configure(app):
-#     #: Configure CORS
-#     origins = [
-#         "http://localhost:3000",
-#         "https://torichan.app",
-#         "https://tori-front.vercel.app",
-#     ]
-#     app.add_middleware(
-#         CORSMiddleware,
-#         allow_origins=origins,
-#         allow_credentials=True,
-#         allow_methods=["*"],
-#         allow_headers=["*"],
-#     )
-#     app.mount("/static", StaticFiles(directory="app/static"), name="static")
+def configure(app):
+    #: Configure CORS
+    origins = [
+        "http://localhost:3000",
+        "https://torichan.app",
+        "https://tori-front.vercel.app",
+    ]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-# def include_router(app):
-#     app.include_router(system.router)
-#     app.include_router(tag.router)
-#     app.include_router(user.router)
-#     app.include_router(word.router)
-#     # app.include_router(admin.router)
+def include_router(app):
+    app.include_router(system.router)
+    app.include_router(tag.router)
+    app.include_router(user.router)
+    app.include_router(word.router)
+    app.include_router(admin.router)
 
-# def start_application():
-#     app = FastAPI()
-#     configure(app)
-#     include_router(app)
-#     return app
+def start_application():
+    app = FastAPI()
+    configure(app)
+    include_router(app)
+    return app
 
-# app = start_application()
+app = start_application()
+
+@app.get("/users")
+def read_users():
+    return {"id": 1, "name": "taro"}
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int):
+    return {"user_id": user_id}
 
 # app = FastAPI()
 
@@ -63,24 +71,24 @@
 # uvicorn app.main:app --host 0.0.0.0 --port 8080 --reload --proxy-headers --forwarded-allow-ips='*'
 
 
-from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware # 追加
+# from fastapi import FastAPI
+# from starlette.middleware.cors import CORSMiddleware # 追加
 
-app = FastAPI()
+# app = FastAPI()
 
-# CORSを回避するために追加（今回の肝）
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,   # 追記により追加
-    allow_methods=["*"],      # 追記により追加
-    allow_headers=["*"]       # 追記により追加
-)
+# # CORSを回避するために追加（今回の肝）
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,   # 追記により追加
+#     allow_methods=["*"],      # 追記により追加
+#     allow_headers=["*"]       # 追記により追加
+# )
 
-@app.get("/users")
-def read_users():
-    return {"id": 1, "name": "taro"}
+# @app.get("/users")
+# def read_users():
+#     return {"id": 1, "name": "taro"}
 
-@app.get("/users/{user_id}")
-def read_user(user_id: int):
-    return {"user_id": user_id}
+# @app.get("/users/{user_id}")
+# def read_user(user_id: int):
+#     return {"user_id": user_id}
